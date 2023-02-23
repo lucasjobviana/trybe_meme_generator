5e24d5e194ea4ccde5744fd0e431fbb7f252df74
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-02-23 19:37:04 -0300

    Adicionando sexto requisito.

--- script.js
+++ script.js
@@ -3,34 +3,39 @@ const iptFrase = document.getElementById('text-input');
 const iptMemeImage = document.getElementById('meme-insert');
 const imgImage = document.getElementById('meme-image');
 const conteiner = document.getElementById('meme-image-container');
-const toolsText = document.getElementById('tools_text');
+const tools = document.getElementById('tools');
 
 iptFrase.addEventListener('keyup', () => {
   spnFrase.textContent = iptFrase.value;
 });
 
-window.onload = () =>{
+window.onload = () => {
 
-  toolsText.addEventListener('click',(evt)=>{
+  tools.addEventListener('click', (evt) => {
     const botao = evt.target;
-    
-    switch(botao.value){
+
+    switch (botao.value) {
       case 'up': conteiner.style.justifyContent = 'flex-start'; break;
       case 'right': conteiner.style.alignItems = 'flex-end'; break;
       case 'down': conteiner.style.justifyContent = 'flex-end'; break;
-      case 'left': conteiner.style.alignItems = 'flex-start';break;
+      case 'left': conteiner.style.alignItems = 'flex-start'; break;
+      case 'fire': conteiner.style.border = '3px dashed rgb(255, 0, 0)'; break;
+      case 'water': conteiner.style.border = '5px double rgb(0, 0, 255)'; break;
+      case 'earth': conteiner.style.border = '6px groove rgb(0, 128, 0)'; break;
+
     }
+
   });
 }
 
 
-iptMemeImage.addEventListener('change',(evt)=>{
-   // Algoritmo desenvolvido com base na aprendizagem adquirida a partir dessas fontes:
+iptMemeImage.addEventListener('change', (evt) => {
+  // Algoritmo desenvolvido com base na aprendizagem adquirida a partir dessas fontes:
   // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
   // https://www.youtube.com/watch?v=gXXmQeZOHKY
   const input = evt.target;
   const file = input.files[0];
-  console.log (input);
+  console.log(input);
   console.log(file);
 
   const reader = new FileReader();
@@ -38,10 +43,10 @@ iptMemeImage.addEventListener('change',(evt)=>{
 
   console.log('vou definir o event load')
   console.log(reader);
-  reader.addEventListener('load',(evt)=>{
+  reader.addEventListener('load', (evt) => {
     console.log('fdsfadfd')
     const readerTarget = evt.target;
-    imgImage.setAttribute('src',readerTarget.result)
+    imgImage.setAttribute('src', readerTarget.result)
   });
 
 });
