ca137e89da70aa4629f17be1f3d3d5f20d9cd2aa
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-02-22 21:11:41 -0300

    Tentando selecionar o arquivo e atribuir ao src da img.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const spnFrase = document.getElementById('meme-text');
 const iptFrase = document.getElementById('text-input');
+const iptMemeImage = document.getElementById('meme-insert');
+const imgImage = document.getElementById('meme-image');
 
 iptFrase.addEventListener('keyup', () => {
   spnFrase.textContent = iptFrase.value;
 });
+
+iptMemeImage.onchange = () => {
+  // imgImage.setAttribute('src', iptMemeImage.value);
+  console.log(iptMemeImage.value);
+  console.log(iptMemeImage.name);
+  console.log(iptMemeImage.files[0].name);
+};
