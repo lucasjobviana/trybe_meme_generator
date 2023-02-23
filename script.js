5ecc6e15c0f7a60b9e9c64c71c173d175fc07e4b
Job1991 <lucasjobvianna@hotmail.com>
2023-02-23 00:33:50 -0300

    Atribuido a imagem selecionada à tag img.

--- script.js
+++ script.js
@@ -2,14 +2,34 @@ const spnFrase = document.getElementById('meme-text');
 const iptFrase = document.getElementById('text-input');
 const iptMemeImage = document.getElementById('meme-insert');
 const imgImage = document.getElementById('meme-image');
+const conteiner = document.getElementById('meme-image-container');
 
 iptFrase.addEventListener('keyup', () => {
   spnFrase.textContent = iptFrase.value;
 });
 
-iptMemeImage.onchange = () => {
-  // imgImage.setAttribute('src', iptMemeImage.value);
-  console.log(iptMemeImage.value);
-  console.log(iptMemeImage.name);
-  console.log(iptMemeImage.files[0].name);
-};
+
+iptMemeImage.addEventListener('change',(evt)=>{
+  const input = evt.target;
+  const file = input.files[0];
+  console.log (input);
+  console.log(file)
+
+    alert('tem arquivo');
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    
+    console.log('vou definir o event load')
+    console.log(reader);
+    reader.addEventListener('load',(evt)=>{
+      console.log('fdsfadfd')
+      const readerTarget = evt.target;
+      imgImage.setAttribute('src',readerTarget.result)
+    });
+
+});
+
+
+
+
+
